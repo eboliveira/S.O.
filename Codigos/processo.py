ff6744637658c2ab3fe3ea175785505d065f111e
eduh1997 <eduardooliveira.1997@alunos.utfpr.edu.br>
2018-04-25 16:40:14 -0300

    Retirado ProcessoRR

--- Codigos/processo.py
+++ Codigos/processo.py
@@ -19,24 +19,3 @@ class Processo(object):
         nome, posi = tup
         return self.__getattribute__(nome)[posi] 
   
-class ProcessoRR(object):
-
-    tempos = {}
-    estado = None
-    eventos = []
-
-    def __init__(self, Id, prioridade, tempos, tam, estado, eventos,quant):
-        self.id = Id
-        self.prioridade = prioridade
-        self.tempos = tempos
-        self.estado = estado
-        self.eventos = eventos
-        self.tamanho = tam 
-        self.quantum = quant
-    def __str__(self):      #funcao de print de um objeto
-        return "\n" + str(self.__dict__)
-    def __repr__(self):     #funcao de print de uma lista de objetos
-        return str(self) + "\n"
-    def __getitem__(self, tup):     #recebe como parametro uma tupla que contem o nome do atributo e posicao; retorna o objeto
-        nome, posi = tup
-        return self.__getattribute__(nome)[posi] 
